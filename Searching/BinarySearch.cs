3792deada7d5a7b800089ee0422dc1d923173a88
jaykant01 <jaykanty24092004@gmail.com>
2026-01-06 15:17:08 +0530

    [Jaykant] Add .Binary Search

---
+++ Searching/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Searching;
+
+public class BinarySearch
+{
+    public int BinaryS()
+    {
+        int[] arr = {2, 4, 5, 7, 9, 10, 12, 14, 16, 18, 20 };
+
+        int st = 0;
+        int end = arr.Length-1;
+        int target = Convert.ToInt32(Console.ReadLine());
+        while(st <= end)
+        {
+            int mid = st + (end - st) / 2;
+
+            if (arr[mid] == target)
+            {
+                return mid;
+
+            }else if (target > arr[mid])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
--- Searching/Program.cs
+++ Searching/Program.cs
@@ -4,9 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Linear Search");
-        LinearSearch ls = new LinearSearch();
-        Console.WriteLine("Target is at Index: " + ls.LinearS());
+        // Linear Search
+        //Console.WriteLine("Linear Search");
+        //LinearSearch ls = new LinearSearch();
+        //Console.WriteLine("Target is at Index: " + ls.LinearS());
+
+
+        // Binary Search
+        Console.WriteLine("Binary Search");
+        BinarySearch bs = new BinarySearch();
+        Console.WriteLine("Target is at Index: " + bs.BinaryS());
+
         Console.ReadKey();
     }
 }
