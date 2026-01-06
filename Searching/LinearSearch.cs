4454ccf06c78252cec0de7c7e0a7caeb39d4edbe
jaykant01 <jaykanty24092004@gmail.com>
2026-01-06 14:51:41 +0530

    [Jaykant] Add .Linear Search

---
+++ Searching/LinearSearch.cs
@@ -0,0 +1,28 @@
+﻿namespace Searching;
+
+public class LinearSearch
+{
+    public int LinearS()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i<n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int target = Convert.ToInt32(Console.ReadLine());
+
+        // Linear Search Logic
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] == target)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Searching/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Searching;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Linear Search");
+        LinearSearch ls = new LinearSearch();
+        Console.WriteLine("Target is at Index: " + ls.LinearS());
+        Console.ReadKey();
+    }
+}
