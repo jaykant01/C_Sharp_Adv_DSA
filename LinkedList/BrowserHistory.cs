f097660c2c1c3f86b81e8d29b4a3db074cf71c85
jaykant01 <jaykanty24092004@gmail.com>
2026-01-07 10:53:41 +0530

    [Jaykant] Add .Browser History Navigation System Design

---
+++ LinkedList/BrowserHistory.cs
@@ -0,0 +1,83 @@
+﻿namespace LinkedList;
+
+public class Node
+{
+    public string data {  get; set; }
+    public Node next { get; set; }
+    public Node prev {  get; set; }
+
+
+    public Node(string data)
+    {
+        this.data = data;
+    }
+}
+
+public class BrowserHistory : Navigation
+{
+    private Node head { get; set; }
+    private Node curr { get; set; }
+
+    // AddPage Method
+    public override void AddFirst(string data)
+    {
+        Node newNode = new Node(data);
+
+        if(curr != null)
+        {
+            curr.next = newNode;
+            newNode.prev = curr;
+        }
+        curr = newNode;
+        Console.WriteLine($"Visited: {curr.data}");
+    }
+
+    // Forward page
+    public override void Forward()
+    {
+        if(curr.next != null)
+        {
+            curr = curr.next;
+            Console.WriteLine($"Forward to: {curr.data}");
+        }
+        else
+        {
+            Console.WriteLine("No next page");
+        }
+    }
+
+    // Backward page
+    public override void Backward()
+    {
+        if( curr.prev != null)
+        {
+            curr = curr.prev;
+            Console.WriteLine($"Backward to: {curr.data}");
+        }
+        else
+        {
+            Console.WriteLine("No previous page");
+        }
+    }
+
+
+    // Display all History
+    public void Display()
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            Console.WriteLine(temp.data + " ");
+            temp = temp.next;
+        }
+        Console.WriteLine();
+    }
+}
+
+public abstract class Navigation
+{
+    public abstract void AddFirst(string item);
+    public abstract void Backward();
+    public abstract void Forward();
+}
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LinkedList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Browser History Navigation
+        Navigation nh = new BrowserHistory();
+        nh.AddFirst("github.com");
+        nh.AddFirst("google.com");
+        nh.AddFirst("stackoverflow.com");
+        nh.AddFirst("epicgames.com");
+
+        BrowserHistory bh = new BrowserHistory();
+        bh.Display();
+        nh.Backward();
+        nh.Backward();
+        nh.Backward();
+        nh.Backward();
+        nh.Forward();
+        nh.Forward();
+        nh.Forward();
+        nh.Forward();
+        
+
+
+        Console.ReadKey();
+    }
+}
