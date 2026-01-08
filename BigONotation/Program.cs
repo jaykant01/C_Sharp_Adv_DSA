d472af660743206704472daa1e481eb305fb0945
jaykant01 <jaykanty24092004@gmail.com>
2026-01-08 12:07:56 +0530

    [Jaykant] Add .Sorting Comparison in Different datasets

--- BigONotation/Program.cs
+++ BigONotation/Program.cs
@@ -7,30 +7,61 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        int[] datasetSizes = { 1000, 10000, 1_000_000 };
-        int target = -1;
+        //int[] datasetSizes = { 1000, 10000, 1_000_000 };
+        //int target = -1;
 
-        foreach (int size in datasetSizes)
+        //foreach (int size in datasetSizes)
+        //{
+        //    int[] data = SearchComparison.GenerateData(size);
+
+        //    Console.WriteLine($"\nDataset Size: {size}");
+
+        //    // Linear Search
+        //    Stopwatch sw = Stopwatch.StartNew();
+        //    SearchComparison.LinearSearch(data, target);
+        //    sw.Stop();
+        //    Console.WriteLine($"Linear Search Time: {sw.ElapsedMilliseconds} ms");
+
+        //    // Binary Search
+        //    Array.Sort(data);
+
+        //    sw.Restart();
+        //    SearchComparison.BinarySearch(data, target);
+        //    sw.Stop();
+        //    Console.WriteLine($"Binary Search Time: {sw.ElapsedMilliseconds} ms");
+        //}
+
+        // Problem 2 Sorting Large Data Efficiently
+        int[] sizes = { 1000, 10000 };
+
+        foreach (int size in sizes)
         {
-            int[] data = SearchComparison.GenerateData(size);
-
             Console.WriteLine($"\nDataset Size: {size}");
 
-            // Linear Search
-            Stopwatch sw = Stopwatch.StartNew();
-            SearchComparison.LinearSearch(data, target);
+            int[] data1 = SortingEfficiently.GenerateData(size);
+            int[] data2 = (int[])data1.Clone();
+            int[] data3 = (int[])data1.Clone();
+
+            Stopwatch sw = new Stopwatch();
+
+            // Bubble Sort
+            sw.Start();
+            SortingEfficiently.BubbleSort(data1);
             sw.Stop();
-            Console.WriteLine($"Linear Search Time: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Bubble Sort: {sw.ElapsedMilliseconds} ms");
 
-            // Binary Search
-            Array.Sort(data);
-
+            // Merge Sort
             sw.Restart();
-            SearchComparison.BinarySearch(data, target);
+            SortingEfficiently.MergeSort(data2);
             sw.Stop();
-            Console.WriteLine($"Binary Search Time: {sw.ElapsedMilliseconds} ms");
-        }
+            Console.WriteLine($"Merge Sort: {sw.ElapsedMilliseconds} ms");
 
+            // Quick Sort
+            sw.Restart();
+            SortingEfficiently.QuickSort(data3, 0, data3.Length - 1);
+            sw.Stop();
+            Console.WriteLine($"Quick Sort: {sw.ElapsedMilliseconds} ms");
+        }
         Console.ReadKey();
     }
 }
---
+++ BigONotation/SortingEfficiently.cs
@@ -0,0 +1,94 @@
+﻿namespace BigONotation;
+
+public class SortingEfficiently
+{
+    public static int[] GenerateData(int size)
+    {
+        Random rnd = new Random();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+            arr[i] = rnd.Next();
+
+        return arr;
+    }
+
+    // Bubble Sort
+    public static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n-1-i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                }  
+            }
+        }
+    }
+
+    // Merge Sort
+    public static void MergeSort(int[] arr)
+    {
+        if (arr.Length <= 1) return;
+
+        int mid = arr.Length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.Length - mid];
+
+        Array.Copy(arr, 0, left, 0, mid);
+        Array.Copy(arr, mid, right, 0, arr.Length - mid);
+
+        MergeSort(left);
+        MergeSort(right);
+        Merge(arr, left, right);
+    }
+
+    private static void Merge(int[] arr, int[] left, int[] right)
+    {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.Length && j < right.Length)
+        {
+            arr[k++] = left[i] <= right[j] ? left[i++] : right[j++];
+        }
+
+        while (i < left.Length)
+            arr[k++] = left[i++];
+
+        while (j < right.Length)
+            arr[k++] = right[j++];
+    }
+
+    // Quick Sort
+    public static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pivotIndex = Partition(arr, low, high);
+            QuickSort(arr, low, pivotIndex - 1);
+            QuickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+        }
+
+        (arr[i + 1], arr[high]) = (arr[high], arr[i + 1]);
+        return i + 1;
+    }
+}
