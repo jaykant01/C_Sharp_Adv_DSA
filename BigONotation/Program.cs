926851d205e202640da21c49fdfe9f11a91ec23b
jaykant01 <jaykanty24092004@gmail.com>
2026-01-08 12:27:51 +0530

    [Jaykant] Add .String Concatenation Performance in Different datasets

--- BigONotation/Program.cs
+++ BigONotation/Program.cs
@@ -32,36 +32,64 @@ class Program
         //}
 
         // Problem 2 Sorting Large Data Efficiently
-        int[] sizes = { 1000, 10000 };
+        //int[] sizes = { 1000, 10000 };
+
+        //foreach (int size in sizes)
+        //{
+        //    Console.WriteLine($"\nDataset Size: {size}");
+
+        //    int[] data1 = SortingEfficiently.GenerateData(size);
+        //    int[] data2 = (int[])data1.Clone();
+        //    int[] data3 = (int[])data1.Clone();
+
+        //    Stopwatch sw = new Stopwatch();
+
+        //    // Bubble Sort
+        //    sw.Start();
+        //    SortingEfficiently.BubbleSort(data1);
+        //    sw.Stop();
+        //    Console.WriteLine($"Bubble Sort: {sw.ElapsedMilliseconds} ms");
+
+        //    // Merge Sort
+        //    sw.Restart();
+        //    SortingEfficiently.MergeSort(data2);
+        //    sw.Stop();
+        //    Console.WriteLine($"Merge Sort: {sw.ElapsedMilliseconds} ms");
+
+        //    // Quick Sort
+        //    sw.Restart();
+        //    SortingEfficiently.QuickSort(data3, 0, data3.Length - 1);
+        //    sw.Stop();
+        //    Console.WriteLine($"Quick Sort: {sw.ElapsedMilliseconds} ms");
+
+        // Problem 3
+        int[] sizes = { 1000, 10000, 1_000_000 };
 
         foreach (int size in sizes)
         {
-            Console.WriteLine($"\nDataset Size: {size}");
-
-            int[] data1 = SortingEfficiently.GenerateData(size);
-            int[] data2 = (int[])data1.Clone();
-            int[] data3 = (int[])data1.Clone();
-
+            Console.WriteLine($"\nOperations Count: {size}");
             Stopwatch sw = new Stopwatch();
 
-            // Bubble Sort
-            sw.Start();
-            SortingEfficiently.BubbleSort(data1);
-            sw.Stop();
-            Console.WriteLine($"Bubble Sort: {sw.ElapsedMilliseconds} ms");
+            // string concatenation (skip for very large input)
+            if (size <= 10000)
+            {
+                sw.Start();
+                StringConcatenationPerformance.UsingString(size);
+                sw.Stop();
+                Console.WriteLine($"string: {sw.ElapsedMilliseconds} ms");
+            }
+            else
+            {
+                Console.WriteLine("string: Skipped (Too Slow)");
+            }
 
-            // Merge Sort
+            // StringBuilder
             sw.Restart();
-            SortingEfficiently.MergeSort(data2);
+            StringConcatenationPerformance.UsingStringBuilder(size);
             sw.Stop();
-            Console.WriteLine($"Merge Sort: {sw.ElapsedMilliseconds} ms");
-
-            // Quick Sort
-            sw.Restart();
-            SortingEfficiently.QuickSort(data3, 0, data3.Length - 1);
-            sw.Stop();
-            Console.WriteLine($"Quick Sort: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"StringBuilder: {sw.ElapsedMilliseconds} ms");
         }
+
         Console.ReadKey();
     }
 }
---
+++ BigONotation/StringConcatenationPerformance.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+
+namespace BigONotation;
+
+public class StringConcatenationPerformance
+{
+    // Using string
+    public static string UsingString(int count)
+    {
+        string result = "";
+
+        for (int i = 0; i < count; i++)
+        {
+            result += "a";
+        }
+
+        return result;
+    }
+
+    // Using StringBuilder
+    public static string UsingStringBuilder(int count)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < count; i++)
+        {
+            sb.Append("a");
+        }
+
+        return sb.ToString();
+    }
+}
