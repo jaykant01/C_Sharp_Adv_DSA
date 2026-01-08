15c1c9365c089e0991d6d9a82c005942d3edae7c
jaykant01 <jaykanty24092004@gmail.com>
2026-01-08 16:16:35 +0530

    [Jaykant] .Add String builder

---
+++ FileHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FileHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // String builder Problem 1
+        ReverseUsingBuilder r = new ReverseUsingBuilder();
+        r.ReverseString();
+
+        Console.ReadKey();
+
+    }
+}
---
+++ FileHandling/ReverseUsingBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace FileHandling;
+
+public class ReverseUsingBuilder
+{
+    public void ReverseString()
+    {
+        StringBuilder sb = new StringBuilder("Stranger Things");
+
+        Console.WriteLine("original String: " + sb);
+        
+        for(int i=sb.Length-1; i>=0; i--)
+        {
+            Console.Write(sb[i]);
+        }
+    }
+}
