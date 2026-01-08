ed25e916ef3a7b615e578e2721fe5efe58174c64
jaykant01 <jaykanty24092004@gmail.com>
2026-01-08 11:40:07 +0530

    [Jaykant] Add .Search Comparison in Different datasets

---
+++ BigONotation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace BigONotation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        int[] datasetSizes = { 1000, 10000, 1_000_000 };
+        int target = -1;
+
+        foreach (int size in datasetSizes)
+        {
+            int[] data = SearchComparison.GenerateData(size);
+
+            Console.WriteLine($"\nDataset Size: {size}");
+
+            // Linear Search
+            Stopwatch sw = Stopwatch.StartNew();
+            SearchComparison.LinearSearch(data, target);
+            sw.Stop();
+            Console.WriteLine($"Linear Search Time: {sw.ElapsedMilliseconds} ms");
+
+            // Binary Search
+            Array.Sort(data);
+
+            sw.Restart();
+            SearchComparison.BinarySearch(data, target);
+            sw.Stop();
+            Console.WriteLine($"Binary Search Time: {sw.ElapsedMilliseconds} ms");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ BigONotation/SearchComparison.cs
@@ -0,0 +1,50 @@
+﻿namespace BigONotation;
+
+public class SearchComparison
+{
+    public static int[] GenerateData(int size)
+    {
+        int[] arr = new int[size];
+        for(int i=0; i<size; i++)
+        {
+            arr[i] = i; 
+        }
+        return arr;
+    }
+
+    public static int LinearSearch(int[] arr, int target)
+    {
+        for(int i=0; i<arr.Length; i++)
+        {
+            if(arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int BinarySearch(int[] arr, int target)
+    {
+        int st = 0;
+        int end = arr.Length;
+
+        while(st <= end)
+        {
+            int mid = st + (end - st) / 2;
+
+            if(arr[mid] == target)
+            {
+                return mid;
+            }else if(target >  arr[mid])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
