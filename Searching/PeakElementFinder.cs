a361680d0ffece101216eab19e024020afcce8f8
jaykant01 <jaykanty24092004@gmail.com>
2026-01-09 14:52:39 +0530

    [Jaykant] .Add Binary Search Peak Element in Array

---
+++ Searching/PeakElementFinder.cs
@@ -0,0 +1,35 @@
+﻿namespace Searching;
+
+public class PeakElementFinder
+{
+    private int[] arr = { 1, 3, 20, 4, 1, 0 };
+
+    public int FindPeakElementIndex()
+    {
+        int low = 0;
+        int high = arr.Length - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    public void PrintPeak()
+    {
+        int peakIndex = FindPeakElementIndex();
+        Console.WriteLine("Peak Element Index: " + peakIndex);
+        Console.WriteLine("Peak Element Value: " + arr[peakIndex]);
+    }
+}
