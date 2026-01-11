3487b18fd58a427740814931df0996858d34914a
jaykant01 <jaykanty24092004@gmail.com>
2026-01-11 21:49:57 +0530

    [Jaykant] Add .Bubble Sort

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿namespace Sorting;
+
+public class BubbleSort
+{
+    public static void Bubble()
+    {
+        int[] marks = { 78, 45, 90, 62, 88, 55 };
+
+        Console.WriteLine("Marks before sorting:");
+        foreach (int mark in marks)
+        {
+            Console.Write(mark + " ");
+        }
+
+        int n = marks.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (marks[j] > marks[j + 1])
+                {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+
+        Console.WriteLine("\n\nMarks after sorting (Ascending Order):");
+        foreach (int mark in marks)
+        {
+            Console.Write(mark + " ");
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Bubble Sort
+        BubbleSort.Bubble();
+
+        Console.ReadKey();
     }
 }
