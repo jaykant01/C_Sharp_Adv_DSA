d4fcef358017f36006adfbb15417c3d478b6a8fe
jaykant01 <jaykanty24092004@gmail.com>
2026-01-11 22:24:22 +0530

    [Jaykant] Add .Stack using Web Browser Tab Navigation

---
+++ Stack/DesktopBrowser.cs
@@ -0,0 +1,60 @@
+﻿namespace Stack;
+
+interface INavigationManager
+{
+    void Visit(string url);
+    void Back();
+    void Forward();
+    void DisplayCurrent();
+}
+
+public class DesktopBrowser : INavigationManager
+{
+    private Stack<string> backStack = new Stack<string>();
+    private Stack<string> forwardStack = new Stack<string>();
+    private string currentPage = null;
+
+    public void Visit(string url)
+    {
+        if (currentPage != null)
+            backStack.Push(currentPage);
+
+        currentPage = url;
+        forwardStack.Clear();
+
+        Console.WriteLine("Visited: " + currentPage);
+    }
+
+    public void Back()
+    {
+        if (backStack.Count == 0)
+        {
+            Console.WriteLine("No pages to go back.");
+            return;
+        }
+
+        forwardStack.Push(currentPage);
+        currentPage = backStack.Pop();
+
+        Console.WriteLine("Back to: " + currentPage);
+    }
+
+    public void Forward()
+    {
+        if (forwardStack.Count == 0)
+        {
+            Console.WriteLine("No pages to go forward.");
+            return;
+        }
+
+        backStack.Push(currentPage);
+        currentPage = forwardStack.Pop();
+
+        Console.WriteLine("Forward to: " + currentPage);
+    }
+
+    public void DisplayCurrent()
+    {
+        Console.WriteLine("Current Page: " + currentPage);
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -5,15 +5,31 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1
-        IExpressionEvaluator evaluator;
+        //IExpressionEvaluator evaluator;
 
-        evaluator = new PostfixEvaluator(); 
+        //evaluator = new PostfixEvaluator(); 
 
-        string expression = "3+5*2";
-        int result = evaluator.Evaluate(expression);
+        //string expression = "3+5*2";
+        //int result = evaluator.Evaluate(expression);
 
-        Console.WriteLine("Expression: " + expression);
-        Console.WriteLine("Result: " + result);
+        //Console.WriteLine("Expression: " + expression);
+        //Console.WriteLine("Result: " + result);
+
+
+
+        //  Problem 2
+        INavigationManager browser;
+
+        browser = new DesktopBrowser();
+
+        browser.Visit("google.com");
+        browser.Visit("github.com");
+        browser.Visit("stackoverflow.com");
+        browser.Back();
+        browser.Back();
+        browser.Forward();
+
+        browser.DisplayCurrent();
 
         Console.ReadKey();
     }
