529c9f5b05c7b25480b856569effbee078487957
jaykant01 <jaykanty24092004@gmail.com>
2026-01-11 22:35:31 +0530

    [Jaykant] Add .Queue using Print Job Manager

---
+++ Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Queue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        PrintQueue printer = new PrintQueue();
+
+        printer.AddJob(new TextDocument("Hello World"));
+        printer.AddJob(new ImageFile("photo.png"));
+        printer.AddJob(new TextDocument("Exam Notes"));
+
+        printer.ProcessNextJob();
+        printer.ProcessNextJob();
+        printer.ProcessNextJob();
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Queue/TextDocument.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace Queue;
+
+interface IPrintable
+{
+    void Print();
+}
+
+
+public class TextDocument : IPrintable
+{
+    private string content;
+
+    public TextDocument(string content)
+    {
+        this.content = content;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Printing text document: " + content);
+    }
+}
+
+class ImageFile : IPrintable
+{
+    private string fileName;
+
+    public ImageFile(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Printing image file: " + fileName);
+    }
+}
+
+class PrintQueue
+{
+    private Queue<IPrintable> queue = new Queue<IPrintable>();
+
+    public void AddJob(IPrintable job)
+    {
+        queue.Enqueue(job);
+        Console.WriteLine("Job added to print queue.");
+    }
+
+    public void ProcessNextJob()
+    {
+        if (queue.Count == 0)
+        {
+            Console.WriteLine("No print jobs in queue.");
+            return;
+        }
+
+        IPrintable job = queue.Dequeue();
+        job.Print();
+    }
+}
