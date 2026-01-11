d71c8084344c1c8bd675b08a3993cd368f428c67
jaykant01 <jaykanty24092004@gmail.com>
2026-01-11 23:26:08 +0530

    [Jaykant] Add .HashMap using URL Shortener Service

---
+++ HashMap/InMemoryURLStorage.cs
@@ -0,0 +1,28 @@
+﻿namespace HashMap;
+
+public interface IURLStorage
+{
+    void PutURL(string shortUrl, string longUrl);
+    string GetURL(string shortUrl);
+}
+
+public class InMemoryURLStorage : IURLStorage
+{
+    private Dictionary<string, string> urlMap = new Dictionary<string, string>();
+
+    public void PutURL(string shortUrl, string longUrl)
+    {
+        urlMap[shortUrl] = longUrl;
+        Console.WriteLine($"Short URL {shortUrl} mapped to {longUrl}");
+    }
+
+    public string GetURL(string shortUrl)
+    {
+        if (urlMap.TryGetValue(shortUrl, out string longUrl))
+        {
+            return longUrl;
+        }
+        Console.WriteLine($"Short URL {shortUrl} not found.");
+        return null;
+    }
+}
--- HashMap/Program.cs
+++ HashMap/Program.cs
@@ -32,13 +32,29 @@ class Program
 
 
         // Problem 3
-        GradeService gradeService = new GradeService();
+        //GradeService gradeService = new GradeService();
 
-        gradeService.AddGrade(101, new CBSEGradeReport());
-        gradeService.AddGrade(102, new GPAGradeReport());
+        //gradeService.AddGrade(101, new CBSEGradeReport());
+        //gradeService.AddGrade(102, new GPAGradeReport());
 
-        gradeService.DisplayGrade(101, 88); 
-        gradeService.DisplayGrade(102, 88);
+        //gradeService.DisplayGrade(101, 88); 
+        //gradeService.DisplayGrade(102, 88);
+
+
+        // Problem 4
+        IURLStorage storage = new InMemoryURLStorage();
+
+        storage.PutURL("abc123", "https://www.example.com/very-long-url");
+        storage.PutURL("xyz789", "https://www.anotherexample.com/page");
+
+        string longUrl1 = storage.GetURL("abc123");
+        Console.WriteLine("Long URL for abc123: " + longUrl1);
+
+        string longUrl2 = storage.GetURL("xyz789");
+        Console.WriteLine("Long URL for xyz789: " + longUrl2);
+
+        string longUrl3 = storage.GetURL("notfound");
+        Console.WriteLine("Long URL for notfound: " + longUrl3);
 
         Console.ReadKey();
     }
