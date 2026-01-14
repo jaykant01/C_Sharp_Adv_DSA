e64e2ff18cb0b6a57fb1007cf3cbb9faae7ec502
jaykant01 <jaykanty24092004@gmail.com>
2026-01-14 10:48:15 +0530

    [Jaykant] Add .Message Printer using Delegates

---
+++ Delegates/EmptyDel.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegates;
+
+public class EmptyDel
+{
+    public delegate void MessagePrinter();
+
+    public static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the application");
+    }
+
+    public static void ErrorMessage()
+    {
+        Console.WriteLine("An Error has Occurred!");
+    }
+
+    public static void SuccessMessage()
+    {
+        Console.WriteLine("Operation completed Successfully!");
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace Delegates;
+﻿using System.Numerics;
+
+namespace Delegates;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Problem 1 by static method call
+        //Maths.MathOperation op;
+
+        //op = Maths.Add;
+        //Console.WriteLine("Add " + op(10, 5));
+
+        //op = Maths.Subtract;
+        //Console.WriteLine("Subtract " + op(10, 5));
+
+        //op = Maths.Multiply;
+        //Console.WriteLine("Multiply " + op(10, 5));
+
+
+        // Problem 1 - Part 2
+        EmptyDel.MessagePrinter printer;
+
+        printer = EmptyDel.WelcomeMessage;
+        printer();
+
+        printer = EmptyDel.ErrorMessage;
+        printer();
+
+        printer = EmptyDel.SuccessMessage;
+        printer();
         
+        
+        
+        Console.ReadKey();
+
     }
 }
