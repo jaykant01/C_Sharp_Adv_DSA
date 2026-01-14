6c42404970db643066ab5bd77c85df1f5e6ece20
jaykant01 <jaykanty24092004@gmail.com>
2026-01-15 00:45:20 +0530

    [Jaykant] Add .Button click using Events

---
+++ Events/Button.cs
@@ -0,0 +1,20 @@
+﻿namespace Events;
+
+public class Button
+{
+    public event Action Click;
+
+    public void OnClick()
+    {
+        Console.WriteLine("Button clicked (inside Button class)");
+        Click?.Invoke();  
+    }
+}
+
+public class ButtonHandler
+{
+    public void HandleClick()
+    {
+        Console.WriteLine("Button click handled in another class");
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Problem 1 
+        Button button = new Button();
+        ButtonHandler handler = new ButtonHandler();
+
+        // Subscribe to event
+        button.Click += handler.HandleClick;
+
+        // Trigger event
+        button.OnClick();
+
+        Console.ReadKey();
     }
 }
