e9cebcd6731a570f408a7b1c506fd405bdc00c6c
jaykant01 <jaykanty24092004@gmail.com>
2026-01-14 10:47:47 +0530

    [Jaykant] Add .MathsOperation using Delegates

---
+++ Delegates/Maths.cs
@@ -0,0 +1,22 @@
+﻿namespace Delegates;
+
+public class Maths
+{
+    public delegate int MathOperation(int x, int y);
+
+    public static int Add(int x, int y)
+    {
+        return x + y; 
+    }
+
+    public static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    public static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+}
