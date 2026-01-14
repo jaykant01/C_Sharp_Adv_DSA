2c5877fc83f972514017fe5355605ab7d412bcb6
jaykant01 <jaykanty24092004@gmail.com>
2026-01-15 00:47:59 +0530

    [Jaykant] Add .Notifier using Events

---
+++ Events/Notifier.cs
@@ -0,0 +1,20 @@
+﻿namespace Events;
+
+public class Notifier
+{
+    public event Action<string> MessageSent;
+
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Notifier: Sending message...");
+        MessageSent?.Invoke(message); 
+    }
+}
+
+public class MessageReceiver
+{
+    public void OnMessageReceived(string message)
+    {
+        Console.WriteLine("Receiver got message: " + message);
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -5,14 +5,25 @@ class Program
     static void Main(string[] args)
     {
         // Problem 1 
-        Button button = new Button();
-        ButtonHandler handler = new ButtonHandler();
+        //Button button = new Button();
+        //ButtonHandler handler = new ButtonHandler();
+
+        //// Subscribe to event
+        //button.Click += handler.HandleClick;
+
+        //// Trigger event
+        //button.OnClick();
+
+
+        // Problem 2
+        Notifier notifier = new Notifier();
+        MessageReceiver receiver = new MessageReceiver();
 
         // Subscribe to event
-        button.Click += handler.HandleClick;
+        notifier.MessageSent += receiver.OnMessageReceived;
 
         // Trigger event
-        button.OnClick();
+        notifier.SendMessage("Hello, this is a custom event message!");
 
         Console.ReadKey();
     }
